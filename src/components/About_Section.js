8429f83711a911999c43e890a23835d015754d74
Raiza <wisdomadebola62@gmail.com>
2023-06-09 23:30:40 +0100

    Updated

--- src/components/About_Section.js
+++ src/components/About_Section.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function About_Section() {
   return (
-    <div>About_Section</div>
+    <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-64 lg:gap-24 font-style">
+        <div className="lg:mt-[35vh] xl:mt-[28vh] relative md:mt-[96vh] mt-[150vh] sm:mt-[130vh] ">
+            <div className=" w-60  xl:w-96 h-[80vh]  bg-slate-400"></div>
+    <img src="https://preview.colorlib.com/theme/hotell/images/about_1.jpg" alt=""  className=' absolute inset-y-32 inset-x-4 md:inset-y-20 md:inset-x-14  lg:inset-x-10 rounded-md drop-shadow-xl  w-[90vw] sm:w-[80vw] md:w-[65vw]'/>
+        </div>
+        <div className="mt-[-25vh] lg:mt-[50vh] xl:mt-[46vh] mx-16 xl:mx-[8.2rem] ">
+            <div className="text-xl font-extrabold py-5">ABOUT HOTELL</div>
+            <div className="flex flex-col items-center text-sm text-slate-500">
+            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+            <span className='block mt-3'>
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            </span>
+            </div>
+            <button className='hidden xl:flex rounded-lg py-2 px-4 mt-8 border-2  hover:bg-white hover:text-black transition duration-300'>Get Started</button>
+        </div>
+    </div>
   )
 }
 
--- src/components/Services_Section.js
+++ src/components/Services_Section.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Services_Section() {
   return (
     <>
-    <div className="pt-48 font-style mt-44 lg:mt-0 bg-slate-50 h-[85vh]">
+    <div className="pt-48 font-style mt-44 lg:mt-0 bg-slate-50 h-[87vh]">
       <div className="text-xl font-extrabold mb-12 ml-4 lg:ml-10 xl:ml-24">OUR SERVICES</div>
-      <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7  mx-14">
+      <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-7  mx-14">
       <div className="services">
         <img src="https://lh3.googleusercontent.com/XzLJ_vViypnP68cT03Te93o0qX8givJRXzoZYREBtXVNy3wM-oj7cGX69eUXc6hLi555PdV6CpOr7zurgj5GHty0_MwER-bJCBTO--C0" alt="" />
         <div className="my-2 text-xl font-bold">Trekking</div>
@@ -25,14 +25,6 @@ function Services_Section() {
       <div className="services">
         <img src="https://lh3.googleusercontent.com/7WJZlfmMu-350-6ZAt43Q7OtSoNbzkIj4idHqO4TcWRhqHUlUTp1CZtc3lf7rbgDDBNkiZdjhZr-9Zk09tGAvVe-zHwMotDns8dltOx8" alt="" />
         <div className="my-2 text-xl font-bold">Suitcase</div>
-        <div className="text-md text-slate-500">
-        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
-        </div>
-        <div className="service_link"><a href="/">LEARN MORE</a></div>
-      </div>
-      <div className="services">
-        <img src="https://lh3.googleusercontent.com/hJtKecQm6Vmi9bLqfcBWYVlN9ynXqrqIKh_HZ336USKYtdZRhCUDyCyzDuXUnnOUDmrEQCHanlH8oO7aOVr4qQ8KktCYwrqKj36TJ-V6" alt="" />
-        <div className="my-2 text-xl font-bold">Island Hoping</div>
         <div className="text-md text-slate-500">
         Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
         </div>
