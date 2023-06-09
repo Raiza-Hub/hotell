c678917a2b00923568386d4dfe5ca38bf608ca30
Raiza <wisdomadebola62@gmail.com>
2023-06-09 08:35:23 +0100

    Routes Created Sucessfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import "./index.css";
+import { Route, Routes } from "react-router";
+import Home from "./routes/Home";
+import Destinations from "./routes/Destinations";
+import Services from "./routes/Services";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
 
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+   <Routes>
+    <Route path="/" element={<Home/>}/>;
+    <Route path="/destination" element={<Destinations/>}/>;
+    <Route path="/service" element={<Services/>}/>;
+    <Route path="/about" element={<About/>}/>;
+    <Route path="/contact" element={<Contact/>}/>
+   </Routes>
+    </>
   );
 }
 
