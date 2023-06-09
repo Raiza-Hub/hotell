42799c1365b2974d13231189d6564c1d22d67728
Raiza <wisdomadebola62@gmail.com>
2023-06-09 12:06:27 +0100

    Introduction created

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <>
+    <div className="flex items-center flex-col font-style  p-20 h-[80vh]">
+       <div className="text-3xl text-center  font-bold w-screen p-14">Once a year go someplace you've never been before.</div> 
+       <button className='border border-black rounded-lg py-2 px-4'>Learn More</button>
+    </div>
+    </>
+  )
+}
+
+export default Intro
