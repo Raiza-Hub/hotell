8259b3925ecec660aa4f8ca9a0c6914430480a3c
Raiza <wisdomadebola62@gmail.com>
2023-06-09 13:46:52 +0100

    Updated Section1

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Section1() {
   return (
     <>
-    <div className="absolute top-[85vh] w-[100%] shadow-xl">
-    <div className="flex justify-around mx-10 m-4 bg-rose-300 p-7 overflow-hidden">
+    <div className="absolute top-[85vh] w-[100%]">
+    <div className="flex justify-around mx-10 m-4 p-7 bg-white overflow-hidden shadow-xl z-10 rounded-sm">
         <input type="text" name="dateArrival" id="dateArrival" placeholder='Date Arrival' className='inputfield' size={25}/>
         <input type="text" name="dateDeparture" id="dateDeparture" placeholder='Date Departure' className='inputfield' size={25}/>
         <input type="text" name="person" id="person" placeholder='# of Person' className='inputfield' size={25}/>
