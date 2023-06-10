579629ec41a01dac4dea1232fa80dd912addb7ab
Raiza <wisdomadebola62@gmail.com>
2023-06-10 12:23:02 +0100

    Footer Created

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="">
+        <div className="">
+            <div className="">Home</div>
+            <a href=""><div className="">About us</div></a>
+            <a href=""><div className="">Places</div></a>
+            <a href=""><div className="">Services</div></a>
+            <a href=""><div className="">Destination</div></a>
+            <a href=""><div className="">Terms</div></a>
+            <a href=""><div className="">Privacy</div></a>
+        </div>
+        <div className="">
+            <div className="">Practice Area</div>
+            <a href=""><div className="">Travel</div></a>
+            <a href=""><div className="">Popular places</div></a>
+            <a href=""><div className="">Destination</div></a>
+            <a href=""><div className="">Tour</div></a>
+            <a href=""><div className="">Family Vacation</div></a>
+        </div>
+        <div className="">
+            <div className="">Services</div>
+            <a href=""><div className="">Tour</div></a>
+            <a href=""><div className="">Swimming</div></a>
+            <a href=""><div className="">Kayak</div></a>
+            <a href=""><div className="">Surfing</div></a>
+        </div>
+        <div className="">
+            <div className="">Contact</div>
+            <a href=""><div className="">43 Raymouth Rd. Baltemoer, London 3910</div></a>
+            <a href=""><div className="">+1(123)-456-7890</div></a>
+            <a href=""><div className="">+1(123)-456-7890</div></a>
+            <a href=""><div className="">info@mydomain.com</div></a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
