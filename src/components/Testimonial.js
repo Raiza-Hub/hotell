827ea3fb52c2d17eb1d2b082bd73367fcfadc906
Raiza <wisdomadebola62@gmail.com>
2023-06-10 10:55:50 +0100

    Testimonial Section Created

---
+++ src/components/Testimonial.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Testimonial() {
+  return (
+    <>
+    <div className="">
+        <div className="">
+            <div className="">TESTIMONIALS</div>
+            <div className="">
+            A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+            </div>
+        </div>
+        <div className="">
+            <img src="https://lh3.googleusercontent.com/WcVo8NsKLf3ntz7KwfOA9hWctFDXDF01Mj38QP7u_w2BmiiGWG48Zwu-VetkKQRIRnFEXT8G-811LLDlCHULVPoqxIOiuF83KtqK0YLPYg" alt="" />
+            <div className="">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</div>
+            <div className="">
+                <img src="https://preview.colorlib.com/theme/hotell/images/person_1.jpg" alt="" />
+                <div className="">John Doe</div>
+                <div className="">Creative Director</div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Testimonial
